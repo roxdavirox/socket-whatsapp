d754b132bc218dc4e8b05af86dc5905494ce1088
roxdavirox <roxdavirox@gmail.com>
2020-05-20 03:00:05 -0300

    refactor user repo

--- src/app/repositories/usersRepository.js
+++ src/app/repositories/usersRepository.js
@@ -5,17 +5,23 @@ function UsersRepository() {
   return {
     async getUserByEmail(email) {
       return new Promise((resolve, reject) => {
-        const resolveFirstUser = (error, users) => {
-          if (error) resolve('user not found error:', error);
+        const getFirstUser = (error, users) => {
+          if (error) {
+            reject(`user not found error: ${error}`);
+            return false;
+          };
           const [user] = users;
-          if (!user) resolve(false);
+          if (!user) {
+            resolve(false);
+            return false;
+          }
           resolve(user);
         };
         rethinkDb
           .table('users')
           .filter({ email })
           .run(global.connection)
-          .then(cursor => cursor.toArray(resolveFirstUser));
+          .then(cursor => cursor.toArray(getFirstUser));
       })
     }
   }
