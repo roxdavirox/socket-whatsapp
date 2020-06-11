2efda90cb325ffdd6ccf1757ab137440fd60f205
roxdavirox <roxdavirox@gmail.com>
2020-06-11 15:34:18 -0300

    refactor azure upload new version

--- src/app/services/azureStorage.js
+++ src/app/services/azureStorage.js
@@ -5,67 +5,37 @@ const azureBlob = require('@azure/storage-blob');
 
 const streamifier = require('streamifier');
 
-const {
-  Aborter,
-  BlockBlobURL,
-  ContainerURL,
-  ServiceURL,
-  SharedKeyCredential,
-  StorageURL,
-  uploadStreamToBlockBlob,
-} = azureBlob;
-
-const STORAGE_ACCOUNT_NAME = process.env.AZURE_STORAGE_ACCOUNT_NAME;
-const ACCOUNT_ACCESS_KEY = process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY;
-console.log('[azure-service] iniciando variaveis de ambiente');
+const { BlobServiceClient, StorageSharedKeyCredential } = azureBlob;
 
 const ONE_MEGABYTE = 1024 * 1024;
 const FOUR_MEGABYTES = 4 * ONE_MEGABYTE;
+const MAX_CONCURRENCY = 20;
 const ONE_MINUTE = 60 * 1000;
+const account = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+const accountAccessKey = process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY;
+console.log('[azure-service] iniciando variaveis de ambiente');
 
 async function uploadImage(file, fileName) {
   return await uploadFile(file, fileName, 'images');
 }
 
 async function uploadFile(file, fileName, containerName) {
-  console.log('[azure-service] nome do container:', containerName);
-  console.log('[azure-service] autenticando azure credentials');
-  const credentials = new SharedKeyCredential(STORAGE_ACCOUNT_NAME, ACCOUNT_ACCESS_KEY);
-  console.log('[azure-service] autenticado com sucesso...');
-  const pipeline = StorageURL.newPipeline(credentials);
-  console.log('[azure-service] criando pipeline do azure blobs');
-  const accountUrl = `https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net`;
-  const serviceURL = new ServiceURL(accountUrl, pipeline);
-  console.log('[azure-service] iniciando serviços azure blob');
-  const aborter = Aborter.timeout(30 * ONE_MINUTE);
-
-  const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName);
-
-  console.log('[azure-service] fileName:', fileName);
-  const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, fileName);
-
+  const sharedKey = new StorageSharedKeyCredential(account, accountAccessKey);
+  const azureUrl = `https://${account}.blob.core.windows.net`;
+  const blob = new BlobServiceClient(azureUrl, sharedKey);
+  const container = blob.getContainerClient(containerName);
+  const block = container.getBlockBlobClient(fileName);
   const stream = streamifier.createReadStream(file);
-  console.log('[azure-service] criando stream');
-  const uploadOptions = {
-    bufferSize: FOUR_MEGABYTES,
-    maxBuffers: 5,
-  };
-  console.log('[azure-service] enviando arquivo');
-  const response = await uploadStreamToBlockBlob(
-    aborter,
-    stream,
-    blockBlobURL,
-    uploadOptions.bufferSize,
-    uploadOptions.maxBuffers,
-  );
-  console.log('[azure-service] arquivo enviado');
-  const { url } = serviceURL;
-  const imageUrl = `${url}${containerName}/${fileName}`;
-  console.log('[azure-service] url da imagem:', imageUrl);
-  return {
-    imageUrl,
-    ...response,
-  };
+  try {
+    await block.uploadStream(stream, FOUR_MEGABYTES, MAX_CONCURRENCY);
+    const url = `${azureUrl}/${containerName}/${fileName}`;
+    return { url };
+  } catch (err) {
+    console.log(
+      `uploadStream failed, requestId - ${err.details.requestId}, statusCode - ${err.statusCode}, errorCode - ${err.details.errorCode}`,
+    );
+    return err;
+  }
 }
 
 module.exports = {
