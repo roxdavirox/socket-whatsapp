67fa4c03e1e425e9e609401dba367bd9b8dce709
roxdavirox <roxdavirox@gmail.com>
2020-06-17 14:14:20 -0300

    fix body parser limit size file upload

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
